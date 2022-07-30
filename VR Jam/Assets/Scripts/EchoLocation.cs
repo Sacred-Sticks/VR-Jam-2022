0ec8e3cfab1f2e606920a6f08c21c149d3fb3626
Lucas Ackman <75407333+Sacred-Sticks@users.noreply.github.com>
2022-07-30 12:02:30 -0500

    Created Visual Indicator

--- VR Jam/Assets/Scripts/EchoLocation.cs
+++ VR Jam/Assets/Scripts/EchoLocation.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class EchoLocation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject lightPrefab;
+    [Space]
+    [SerializeField] private int[] lightLayers;
+    [Space]
+    [SerializeField] private float red;
+    [SerializeField] private float green;
+    [SerializeField] private float blue;
+    [Space]
+    [SerializeField] private float speed;
+
+    GameObject lightObject;
+    Light lightComponent;
+    Rigidbody bodyComponent;
+    Quaternion rotation;
+    Color color;
+
+    float x;
+    float y;
+
+    private void Start()
     {
-        
+        red /= 255;
+        green /= 255;
+        blue /= 255;
+        color = Color.red * red + Color.blue * blue + Color.green * green;
+        CreateVisuals();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CreateVisuals()
     {
-        
+        for (int i = 0; i < lightLayers.Length; i++)
+        {
+            x = 180 / (lightLayers.Length - 1) * i - 90;
+            for (int j = 0; j < lightLayers[i]; j++)
+            {
+                y = 360 / (lightLayers[i]) * j;
+                rotation = Quaternion.Euler(x, y, 0);
+                lightObject = Instantiate(lightPrefab, transform.position, rotation);
+                lightComponent = lightObject.GetComponent<Light>();
+                bodyComponent = lightObject.GetComponent<Rigidbody>();
+                lightComponent.color = color;
+                bodyComponent.velocity = lightObject.transform.forward * speed;
+            }
+        }
     }
 }
